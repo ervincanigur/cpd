8c70b174ea8d0d390ffd5e370b886a2be0e535ed
Ervin Canigur <ervin@canigur.com>
2023-03-08 00:59:27 -0600

    add file upload and file read

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,15 @@ from __future__ import annotations
 
 from typing import Any
 
+from fastapi import Depends
 from fastapi import FastAPI
+from fastapi import File
+from fastapi import HTTPException
 from fastapi import UploadFile
+from sqlalchemy.orm import Session
 
+from .database import get_session
+from .schemas import File as FileSchema
 from .schemas import FileContent
 from .schemas import FileUpload
 
@@ -17,10 +23,22 @@ app = FastAPI(
 
 
 @app.post("/upload_file", response_model=FileUpload)
-def upload_file(file: UploadFile) -> Any:
-    pass
+def upload_file(
+    session: Session = Depends(get_session),
+    files: list[UploadFile] = File(...),
+) -> Any:
+    uploaded_files: list[FileUpload] = []
+    for file in files:
+        new_file: FileSchema = FileSchema(name=file.filename, content=file.file.read())
+        session.add(new_file)
+        upload_file.append(new_file.id)
+    session.commit()
+    return uploaded_files
 
 
 @app.get("/file_content", response_model=FileContent)
-def get_file_content(id: int) -> Any:
-    pass
+def get_file_content(id: int, session: Session = Depends(get_session)) -> Any:
+    file: FileSchema = session.query(FileSchema).filter(FileSchema.id == id).first()
+    if file is None:
+        raise HTTPException(status_code=404, detail="File not found")
+    return FileContent(name=file.name, content=file.content)
